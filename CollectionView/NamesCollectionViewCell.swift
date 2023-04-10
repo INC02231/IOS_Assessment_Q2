ca0ef9961b57968a1904cd1a3f465201f2103b07
INC02231 <anup.ghosh@incture.com>
2023-04-10 15:44:55 +0530

    Question2

---
+++ CollectionView/NamesCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  FruitsCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Incture on 09/04/23.
+//
+
+import UIKit
+
+class NamesCollectionViewCell: UICollectionViewCell {
+
+    static let reuseIdentifier = "NamesCollectionViewCell"
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var uview: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.uview.layer.cornerRadius = 20.0
+        self.uview.layer.borderColor = UIColor.black.cgColor
+        self.uview.layer.borderWidth = 0.5
+        self.uview.layer.shadowColor = UIColor.black.cgColor
+//        self.uview.layer.shadowOpacity = 0.3
+    }
+
+}
---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Incture on 09/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    let fruits = ["Anup", "Laxmikanta", "Rabi", "Debjeet", "Susmita", "Subhasis", "Sourav", "Abhijeet", "Apurba", "Lipika", "Sathi"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        register()
+    }
+    func register(){
+        let nib = UINib(nibName: "NamesCollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "NamesCollectionViewCell")
+                
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NamesCollectionViewCell", for: indexPath) as! NamesCollectionViewCell
+        cell.name.text = fruits[indexPath.row]
+        return cell
+    }
+
+}
+
